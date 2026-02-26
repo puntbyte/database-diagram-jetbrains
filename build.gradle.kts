366f10102efc244f53de386fd1bdc3c1387bc7cc
Nathnael <nathnael.demere@gmail.com>
2026-02-26 10:24:09 +0300

    Configure IntelliJ Platform Plugin and Node.js Gradle
    - Update IntelliJ Platform plugin to use local IDE installation.
    - Set JVM target to version 21.
    - Configure the buildWebview task to depend on npmInstall and define input/output directories for caching.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,80 +1,89 @@
+import org.jetbrains.intellij.platform.gradle.tasks.RunIdeTask
+
 plugins {
-    id("java")
-    id("org.jetbrains.kotlin.jvm") version "2.1.20"
-    id("org.jetbrains.intellij.platform") version "2.10.2"
-    id("com.github.node-gradle.node") version "7.1.0"
+  id("java")
+  id("org.jetbrains.kotlin.jvm") version "2.1.20"
+  id("org.jetbrains.intellij.platform") version "2.10.2"
+  id("com.github.node-gradle.node") version "7.1.0"
 }
 
 group = "com.puntbyte"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
-    intellijPlatform {
-        defaultRepositories()
-    }
+  mavenCentral()
+  intellijPlatform {
+    defaultRepositories()
+  }
 }
+
+val localIdePath = "C:\\Program Files\\JetBrains\\IntelliJ IDEA 2025.3.3"
 
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
 dependencies {
-    intellijPlatform {
-        intellijIdea("2025.2.4")
-        testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
+  intellijPlatform {
+    //intellijIdea("2025.2.4")
+
+    // Use your local IDE installation instead of downloading one
+    local(localIdePath)
+
+    // keep the testFramework declaration here if you need it
+    testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
 
 
-        // Add plugin dependencies for compilation here, example:
-        // bundledPlugin("com.intellij.java")
+    // Add plugin dependencies for compilation here, example:
+    // bundledPlugin("com.intellij.java")
+  }
 
-        // Add jackson-kotlin module used by jacksonObjectMapper()
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
-    }
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
 }
 
 intellijPlatform {
-    pluginConfiguration {
-        ideaVersion {
-            sinceBuild = "252.25557"
-        }
+  pluginConfiguration {
+    ideaVersion {
+      sinceBuild = "252.25557"
+    }
 
-        changeNotes = """
+    changeNotes = """
             Initial version
         """.trimIndent()
-    }
+  }
 }
 
 node {
-    version.set("22.22.0")
-    download.set(true)
-    workDir.set(file("${project.projectDir}/.gradle/nodejs"))
-    nodeProjectDir.set(file("web")) // Point to our webview folder
+  version.set("22.22.0")
+  download.set(true)
+  workDir.set(file("${project.projectDir}/.gradle/nodejs"))
+  nodeProjectDir.set(file("web")) // Point to our webview folder
 }
 
 tasks {
-    // Set the JVM compatibility versions
-    withType<JavaCompile> {
-        sourceCompatibility = "21"
-        targetCompatibility = "21"
-    }
 
-    val buildWebview by registering(com.github.gradle.node.npm.task.NpmTask::class) {
-        dependsOn(npmInstall) // Ensure dependencies are installed
-        args.set(listOf("run", "build"))
+  // Set the JVM compatibility versions
+  withType<JavaCompile> {
+    sourceCompatibility = "21"
+    targetCompatibility = "21"
+  }
 
-        // Caching: Only rebuild if these files change
-        inputs.dir(file("web/src"))
-        inputs.file(file("web/package.json"))
-        inputs.file(file("web/vite.config.ts"))
-        inputs.file(file("web/index.html"))
-        outputs.dir(file("src/main/resources/web"))
-    }
+  val buildWebview by registering(com.github.gradle.node.npm.task.NpmTask::class) {
+    dependsOn(npmInstall) // Ensure dependencies are installed
+    args.set(listOf("run", "build"))
 
-    processResources {
-        dependsOn(buildWebview)
-    }
+    // Caching: Only rebuild if these files change
+    inputs.dir(file("web/src"))
+    inputs.file(file("web/package.json"))
+    inputs.file(file("web/vite.config.ts"))
+    inputs.file(file("web/index.html"))
+    outputs.dir(file("src/main/resources/web"))
+  }
+
+  processResources {
+    dependsOn(buildWebview)
+  }
 }
 
 kotlin {
-    compilerOptions {
-        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
-    }
+  compilerOptions {
+    jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+  }
 }
